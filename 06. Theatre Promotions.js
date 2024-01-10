00740dcfb5d2bdf0ab215c38baa428e43bc562f3
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-10 16:52:12 +0200

    Add files via upload

---
+++ 06. Theatre Promotions.js
@@ -0,0 +1,37 @@
+function theatrePromotions(input) {
+    let dayType = input[0];
+    let age = Number(input[1]);
+    
+    if (age <= 122 && age >=0) {
+        if (dayType == "Weekday") {
+            if (age >= 0 && age <= 18) {
+                console.log ("12$")
+            } else if (age > 18 && age <= 64) {
+                console.log ("18$")
+            } else if (age > 64 && age <= 122) {
+                console.log ("12$")
+            }
+        } else if (dayType == "Weekend") {
+            if (age >= 0 && age <= 18) {
+                console.log ("15$")
+            } else if (age > 18 && age <= 64) {
+                console.log ("20$")
+            } else if (age > 64 && age <= 122) {
+                console.log ("15$")
+            }
+        } else if (dayType == "Holiday") {
+            if (age >= 0 && age <= 18) {
+                console.log ("5$")
+            } else if (age > 18 && age <= 64) {
+                console.log ("12$")
+            } else if (age > 64 && age <= 122) {
+                console.log ("10$")
+            }
+        }
+        
+    } else {
+        console.log("Error!")
+    }
+
+}
+theatrePromotions("Weekday", -12)
